27c176a404246f73c3645879832ce974c9b5013c
Arijeet Roy <blakelabs57@gmail.com>
2025-08-20 21:44:23 +0530

    First daya of CPP restart

---
+++ circumference.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+int main() {
+    const double PI = 3.14;
+    double radius = 4.5;
+    double circumference = 2 * PI * radius;
+
+    std::cout<<"The circumference is " << circumference;
+    return 0;
+}
---
+++ hello.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+
+int main() {
+    std::cout << "I like burgers !" << std::endl;
+    std::cout << "I like pizza too";
+    return 0;
+}
